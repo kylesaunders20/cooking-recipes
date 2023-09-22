79e1d48329edc06f298a26a47e25de7687646050
Kylesaunders20 <kyle_saunders20@hotmail.com>
2023-09-22 13:48:48 -0230

    Set up basic form for adding a new recipe

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function AddRecipe() {
+    const [formData, setFormData] = useState({
+        name: '',
+        ingredients: '',
+        directions: '',
+        description: '',
+        image: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData(prevState => ({ ...prevState, [name]: value }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
         <div>
             <h2>Add New Recipe</h2>
-            {/* Placeholder for add new recipe form */}
+            <form onSubmit={handleSubmit}>
+                <input type="text" name ="name" placeholder='Recipe Name'
+                value={formData.name} onChange={handleChange} />
+                <textarea name="ingredients" placeholder='Ingredients (comma-separated)'
+                value={formData.ingredients} onChange={handleChange} />
+                <textarea name="directions" placeholder='Directions'
+                value={formData.directions} onChange={handleChange} />
+                <textarea name="description" placeholder='Description'
+                value={formData.description} onChange={handleChange} />
+                <select name="image" value={formData.image} onChange={handleChange}>
+                    <option value="">Select an Image</option>
+                    <option value="cooking-recipes\src\assets\SpaghettiCarbonara.jpg">Placeholder 1</option>
+                </select>
+                <button type="submit">Add Recipe</button>
+            </form>
         </div>
     );
 }
